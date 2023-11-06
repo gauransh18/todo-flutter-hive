4b410acaa590e9fc723a5e0b90bc405ca1fcf52e
gauransh18 <66783709+gauransh18@users.noreply.github.com>
2023-11-07 03:48:21 +0530

    added confetti effect

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -3,6 +3,7 @@ import 'package:hive/hive.dart';
 import 'package:todo_hive/db/db.dart';
 import 'package:todo_hive/util/dialogBox.dart';
 import 'package:todo_hive/util/todo_tile.dart';
+import 'package:confetti/confetti.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,38 +13,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   final _box = Hive.box('todo');
 
   ToDoDB db = ToDoDB();
 
-
   @override
   void initState() {
     super.initState();
-    if (_box.get('todo') == null) {  //first time app is opened
+    if (_box.get('todo') == null) {
+      //first time app is opened
       db.createInitialData();
       // db.updateDatabase();
     } else {
       db.loadData();
     }
-
-
   }
 
   final _controller = TextEditingController();
-
-  
+  final _confettiController =
+      ConfettiController(duration: const Duration(seconds: 5));
 
   void checkBoxChanged(bool? value, int index) {
     setState(() {
       db.toDoList[index][1] = !db.toDoList[index][1];
+
+      if (db.toDoList.every((task) => task[1] == true)) {
+        _confettiController.play();
+      }
     });
     db.updateDatabase();
   }
 
   //add new task
-  void addNewTask(){
+  void addNewTask() {
     setState(() {
       db.toDoList.add([_controller.text, false]);
       _controller.clear();
@@ -51,8 +53,6 @@ class _HomePageState extends State<HomePage> {
     });
     db.updateDatabase();
   }
-
-
 
   //create new task
   void createNewTask() {
@@ -69,7 +69,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   //delete task
-  void deleteTask(int index){
+  void deleteTask(int index) {
     setState(() {
       db.toDoList.removeAt(index);
     });
@@ -77,30 +77,56 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void dispose() {
+    _confettiController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blue[200],
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.blue[400],
-          title: const Text('To-Do'),
-          elevation: 0,
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: createNewTask,
-          child: Icon(Icons.add),
-        ),
-        body: ListView.builder(
+      backgroundColor: Colors.blue[200],
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue[400],
+        title: const Text('To-Do'),
+        elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
+      ),
+      body: Stack(
+        children: [
+          ListView.builder(
             itemCount: db.toDoList.length,
             itemBuilder: (context, index) {
               return ToDoTile(
-                taskName: db.toDoList[index][0],
-                taskStatus: db.toDoList[index][1],
-                onChanged: (value) => checkBoxChanged(value, index),
-                deleteFunction: (context){
-                  deleteTask(index);
-                }
-              );
-            }));
+                  taskName: db.toDoList[index][0],
+                  taskStatus: db.toDoList[index][1],
+                  onChanged: (value) => checkBoxChanged(value, index),
+                  deleteFunction: (context) {
+                    deleteTask(index);
+                  });
+            },
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: ConfettiWidget(
+              confettiController: _confettiController,
+              blastDirectionality: BlastDirectionality.explosive,
+              shouldLoop: false,
+              colors: const [
+                Colors.green,
+                Colors.blue,
+                Colors.pink,
+                Colors.orange,
+                Colors.purple
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
